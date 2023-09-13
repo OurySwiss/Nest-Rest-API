efff58beaebfdbb7a7b28074d3c99b6594292e3e
Yannick Andres <yannick.andres@swisscom.com>
2023-09-13 15:08:03 +0200

    feat: hardcoded item interfce

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './interfaces/item.interface';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  private readonly items: Item[] = [
+    {
+      id: '83982499842988',
+      name: 'Item One',
+      qty: 100,
+      description: 'This is item one',
+    },
+    {
+      id: '20932948582',
+      name: 'Item Two',
+      qty: 50,
+      description: 'This is item two',
+    },
+  ];
+}

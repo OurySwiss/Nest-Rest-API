03497de2febf517f673e6ec0ef2f244ea8292d14
Yannick Andres <yannick.andres@swisscom.com>
2023-09-18 13:14:59 +0200

    feat: delete method for mongoDB

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -16,13 +16,13 @@ export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
 
   @Get()
-  async findAll(): Promise<Item[]> {
-    return await this.itemsService.findAll();
+  findAll(): Promise<Item[]> {
+    return this.itemsService.findAll();
   }
 
   @Get(':id')
-  async findOne(@Param('id') id): Promise<Item> {
-    return await this.itemsService.findOne(id);
+  findOne(@Param('id') id): Promise<Item> {
+    return this.itemsService.findOne(id);
   }
 
   @Post()
@@ -31,8 +31,8 @@ export class ItemsController {
   }
 
   @Delete(':id')
-  delete(@Param('id') id): string {
-    return `Delete ${id}`;
+  delete(@Param('id') id): Promise<Item> {
+    return this.itemsService.delete(id);
   }
 
   @Put(':id')

e3bd3002c0683d4e670e93c46ccef7bf7ac0f4bf
yannick andres <yannick.andres@swisscom.com>
2023-10-03 13:47:32 +0200

    feat: All routes should be working fine now

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -7,33 +7,40 @@ import {
   Param,
   Body,
   NotFoundException,
+  UseGuards,
 } from '@nestjs/common';
 import { BooksService } from './books.service';
+import { JwtAuthGuard } from '../users/JwtAuthGuard';
 
 @Controller('books')
 export class BooksController {
   constructor(private readonly booksService: BooksService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.booksService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: number) {
     return this.booksService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() bookData: any) {
     return this.booksService.create(bookData);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
   update(@Param('id') id: number, @Body() updateData: any) {
     return this.booksService.update(id, updateData);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: number) {
     return this.booksService.remove(id);
